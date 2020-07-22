0bda6b4366a180633ccdc3f0686f53ef4e5cb094
nabitener <na_bitener@hotmail.com>
2020-07-22 18:28:58 -0300

    Rotas ok

---
+++ src/urlsUtils.js
@@ -0,0 +1,6 @@
+export const urls = {
+    login: { name: 'Login', path: '/'},
+    register: { name: 'Register', path: '/register'},
+    kitchen: { name: 'Kitchen', path: '/kitchen'},
+    hall: { name: 'Hall', path: '/hall'}
+}
