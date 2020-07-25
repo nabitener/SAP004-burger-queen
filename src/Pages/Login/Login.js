01b5a20858c27523cc8d0b53eade4634cdb83447
nabitener <na_bitener@hotmail.com>
2020-07-25 17:27:25 -0300

    atualização login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,57 +3,73 @@ import Button from '../../Components/Buttons/index.js';
 import Input from '../../Components/Inputs/index.js';
 import { firebaseAuth } from '../../firebaseUtils';
 import { firebaseStore } from '../../firebaseUtils';
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { urls } from '../../urlsUtils';
 import './style.css';
 import '../../reset.css';
 
-function Login () {
+function Login() {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [userName, setUserName] = useState('');
   const [userId, setUserId] = useState('');
 
-  firebaseAuth.onAuthStateChanged(function(user) {
+  const history = useHistory();
+
+  firebaseAuth.onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
-      setUserName(user.displayName)
-      setUserId(user.uid)
-      console.log (userId)
-      console.log (userName)
+      setUserName(user.displayName);
+      setUserId(user.uid);
+      console.log(userId);
+      console.log(userName);
     } else {
       // User is signed out.
-      setUserName('')
-      setUserId('')
+      setUserName('');
+      setUserId('');
     }
   });
 
-  function userStatus (userId) {
-    firebaseStore.collection("users").where("userId", "==", userId).get()
-    .then(doc => {
-      return(doc)
-    })
+  function userStatus() {
+    const userId = firebaseAuth.currentUser.uid;
+    firebaseStore
+      .collection('users')
+      .doc(userId)
+      .get()
+      .then((doc) => {
+        console.log(doc.data().serviceArea);
+        if (doc.data().serviceArea === 'Salão') {
+          history.push(urls.hall.path);
+        } else {
+          history.push(urls.kitchen.path);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
-  function loginCall (e) {
+  function loginCall(e) {
     e.preventDefault();
-    signIn(email, pass)
+    signIn(email, pass);
   }
 
-  function signIn (email, pass) {
-    firebaseAuth.signInWithEmailAndPassword(email, pass)
-    // .then(res => {setUserId(res.user.uid)
-    //   console.log(userStatus(userId)) 
-    // })
-    .catch(error => {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-    });
+  function signIn(email, pass) {
+    firebaseAuth
+      .signInWithEmailAndPassword(email, pass)
+      .then(() => {
+        userStatus();
+        console.log('OK');
+      })
+      .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
   }
 
-  return(
+  return (
     <form className="form-login">
       <div className="welcome-container-login">
         <h2 className="welcome-message">Bem Vindo à Burger Queen!</h2>
@@ -62,34 +78,30 @@ function Login () {
       <Input
         type="text"
         id="input-email"
-        className={"input-login"}
+        className={'input-login'}
         placeholder="Digite seu e-mail"
         value={email}
         onChange={(e) => setEmail(e.currentTarget.value)}
         required
       />
-      
+
       <Input
         type="password"
         id="input-password"
-        className={"input-login"}
+        className={'input-login'}
         placeholder="Digite sua senha"
         value={pass}
         onChange={(e) => setPass(e.currentTarget.value)}
         required
       />
       <div className="container-button-message">
-        <Button 
-          id="submit-button"
-          onClick={loginCall}
-          name="Entrar"
-        />
+        <Button id="submit-button" onClick={loginCall} name="Entrar" />
         <p className="login-message">
           Não tem registro? <Link to={urls.register.path}>Registre-se!</Link>
         </p>
       </div>
     </form>
-  )
+  );
 }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { urls } from './urlsUtils';
 import * as serviceWorker from './serviceWorker';
 import Register from './Pages/Register/Register.js';
 import Kitchen from './Pages/Kitchen/kitchen.js';
 import Hall from './Pages/Hall/hall.js';
+import Login from './Pages/Login/Login.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route exact path={urls.login.path} component={App} />
+        <Route exact path={urls.login.path} component={Login} />
         <Route path={urls.register.path} component={Register} />
         <Route path={urls.kitchen.path} component={Kitchen} />
         <Route path={urls.hall.path} component={Hall} />
