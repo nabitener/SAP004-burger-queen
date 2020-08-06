725f7d69574e4e094bbbcbbe82a9d2adcea638cf
nabitener <na_bitener@hotmail.com>
2020-08-06 14:22:29 -0300

    Função total, mais, menos e quantidade

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import Input from '../Inputs/index';
 import './style.css';
-
+import Button from '../Buttons';
 
 const Card = (props) => {
   return (
-    <button
-      id={props.idCard}
-      className="classCard"
-      name={props.name}
-      value={props.price}
-      onClick={props.handleclick}
-    >
+    <div key={props.name} id={props.idCard} className="classCard">
       <div className="container-item">
-        
         <figure className="container-icons">
-          <img className='class-icon' src={require('../../Image/Menu_Icons/'+ props.item_name +'.png')} alt="icon-item"></img>
+          <img
+            className="class-icon"
+            src={require('../../Image/Menu_Icons/' + props.item_name + '.png')}
+            alt="icon-item"
+          ></img>
           {/* <img className='class-icon' src={require('../../Image/Menu_Icons/' + props.item_name + '.png')} alt="icon-item"></img> */}
         </figure>
 
-        <div className='container-item-price-options'>
-          
-          <div className='container-item-price'>
-            <span className='class-item'>{props.item_name}</span>
-            <span className='price'>R$ {props.price}</span>
-          </div> 
-
-          <div className='container-options-value'>
-            <label className="highlights-button" htmlFor="modal-menu">Opções</label>
-            <label className="options-description" > XXXXXX</label>
+        <div className="container-item-price-options">
+          <div className="container-item-price">
+            <span className="class-item">{props.item_name}</span>
+            <span className="price">R$ {props.price}</span>
           </div>
-
+          <div className="container-options-value">
+          <Button
+            name="Adicionar"
+            id={props.name}
+            value={props.price}
+            onClick={props.handleclick}
+          />
+            <label className="highlights-button" htmlFor="modal-menu">
+              Opções
+            </label>
+            <label className="options-description"> XXXXXX</label>
+          </div>
         </div>
-
       </div>
-
-
 
       <Input type="checkbox" id="modal-menu" />
       <div className="modal">
@@ -49,9 +48,8 @@ const Card = (props) => {
           <label htmlFor={props.idSabores}></label>
         </div>
         <label className="modal-close" htmlFor="modal-menu"></label>
-        
       </div>
-    </button>
+    </div>
   );
 };
 
