fee7623e565f38d9054d2786772470e78811e370
nabitener <na_bitener@hotmail.com>
2020-08-28 18:15:35 -0300

    Colocando os adicionais dentro de cada hamburger

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import './style.css';
-import Button from '../Buttons';
+import React, { useState } from "react";
+import "./style.css";
+import Button from "../Buttons";
+import Input from "../Inputs";
 
 const Card = (props) => {
-  const [opcao, setOpcao] = useState('');
+  const [opcao, setOpcao] = useState("");
+  const [adicional, setAdicional] = useState("");
 
   return (
     <div id={props.idCard} className="classCard">
@@ -11,7 +13,7 @@ const Card = (props) => {
         <figure className="container-icons">
           <img
             className="class-icon"
-            src={require('../../Image/Menu_Icons/' + props.name + '.png')}
+            src={require("../../Image/Menu_Icons/" + props.name + ".png")}
             alt="icon-item"
           ></img>
         </figure>
@@ -21,36 +23,45 @@ const Card = (props) => {
             <span className="class-item">{props.name}</span>
             <span className="price">R$ {props.price}</span>
           </div>
-
-          <div className="container-options-value-button">
-            <div className="container-options-value">
-              {props.options && (
-                <select
-                  className={['select', props.name].join(' ')}
-                  onChange={(e) => setOpcao(e.currentTarget.value)}
-                >
-                  <option selected disabled>
-                    Opções
-                  </option>
-                  {props.options.map((element) => (
-                    <option value={element} key={props.name + element}>
-                      {element}
-                    </option>
-                  ))}
-                </select>
-              )}
-            </div>
-
-            <div className="add-button-container">
-              <Button
-                name="Add"
-                id={`${props.name}  ${opcao}`}
-                value={props.price}
-                className="add-button"
-                onClick={props.handleclick}
-              />
-            </div>
+          <div className="add-button-container">
+            <Button
+              name="Add"
+              id={`${props.name}  ${opcao} ${adicional}`}
+              value={props.price}
+              className="add-button"
+              onClick={props.handleclick}
+            />
           </div>
+        </div>
+      </div>
+      <div className="container-option-extra">
+        <div className="container-options-value-button">
+          <div className="container-options-value">
+            {props.options && (
+              <select
+                className={["select", props.name].join(" ")}
+                onChange={(e) => setOpcao(e.currentTarget.value)}
+              >
+                <option selected disabled>
+                  Opções
+                </option>
+                {props.options.map((element) => (
+                  <option value={element} key={props.name + element}>
+                    {element}
+                  </option>
+                ))}
+              </select>
+            )}
+          </div>
+        </div>
+        <div className="container-options-value-extra">
+          {props.extra &&
+            props.extra.map((element) => (
+              <label key={props.name + element}>
+                {element}
+                <Input type="checkbox" name={element} value ={element} onChange={(e) => setAdicional(e.currentTarget.value)}></Input>
+              </label>
+            ))}
         </div>
       </div>
     </div>
